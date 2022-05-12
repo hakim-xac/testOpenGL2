0fb08a4d8a258e5132106e710aa149c9433413f1
Андрей Хакимов <hakim.xac@gmail.com>
2022-05-12 20:59:14 +0700

    create triangle

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <string>
+#include <string_view>
 
 
 GLfloat point[]{
@@ -15,7 +17,25 @@ GLfloat color[]{
     0.0f, 0.0f, 1.0f,
 };
 
+const char* const vertex_shader{ 
+    "#version 460\n"
+    "layout(location = 0) in vec3 vertex_position;"
+    "layout(location = 1) in vec3 vertex_color;"
+    "out vec3 color;"
+    "void main() {"
+    "   color = vertex_color;"
+    "   gl_Position = vec4(vertex_position, 1.0);"
+    "}"
+};
 
+const char* const fragment_shader { 
+    "#version 460\n"
+    "in vec3 color;"
+    "out vec4 frag_color;"
+    "void main() {"
+    "   frag_color = vec4(color, 1.0);"
+    "}"
+};
 
 
 
@@ -27,7 +47,7 @@ void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height) {
 }
 
 
-void processInput(GLFWwindow* pWindow) {
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode) {
     if (glfwGetKey(pWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(pWindow, true);
     }
@@ -57,6 +77,7 @@ int main(void)
     }
 
     glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
+    glfwSetKeyCallback(pWindow, glfwKeyCallback);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(pWindow);
@@ -70,13 +91,54 @@ int main(void)
 	std::cout << "OpenGl version: " << glGetString(GL_VERSION) << std::endl;
 
 	glClearColor(0.3f, 0.3f, 0.5f, 1);
+
+    GLuint vs{ glCreateShader(GL_VERTEX_SHADER) };
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs{ glCreateShader(GL_FRAGMENT_SHADER) };
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    GLuint shaderProgram{ glCreateProgram() };
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint points_vbo{};
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+    GLuint colors_vbo{};
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+
+    GLuint vao{};
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pWindow))
     {
-        processInput(pWindow);
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
 
